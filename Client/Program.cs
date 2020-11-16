a440baa5fe4d28967ac796768cbe89e356b077da
Lawrence Ong <lawrenceong@hotmail.com>
2020-11-16 14:59:36 -0500

    grpc sample service, request and stream request

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace gRpc01.Client
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var channel = GrpcChannel.ForAddress("https://localhost:5001");
+      var client = new Characters.CharactersClient(channel);
+      if (1 == 2)
+      {
+        var response = await client.GetCharacterAsync(new CharacterRequest { Id = 3 });
+        Console.WriteLine($"character is: {response.Character.FirstName} {response.Character.LastName}");
+      }
+      var request = client.SearchCharacters(new SearchRequest { Query = "s" });
+      await foreach(var response in request.ResponseStream.ReadAllAsync())
+      {
+        Console.WriteLine($"charater is: {response.Character.FirstName} {response.Character.LastName}");
+			}
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Server/CharacterService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gRpc01.Server
+{
+	public class CharacterService : gRpc01.Characters.CharactersBase
+	{
+
+		public static IEnumerable<Character> Characters = new List<Character>
+		{ 
+			new Character {Id=1, FirstName="f1", LastName= "l1", Show="s1"},
+			new Character {Id=2, FirstName="f2", LastName= "l2", Show="s2"},
+			new Character {Id=3, FirstName="f3", LastName= "l3", Show="s3"},
+			new Character {Id=4, FirstName="f4", LastName= "l4", Show="s4"},
+			new Character {Id=5, FirstName="f5", LastName= "l5", Show="s5"}
+		};
+
+		public override Task<CharacterResponse> GetCharacter(CharacterRequest request, ServerCallContext context)
+		{
+			var character = Characters.FirstOrDefault(i => i.Id == request.Id);
+			return Task.FromResult(new CharacterResponse { Character = character });
+			// return base.GetCharacter(request, context);
+		}
+
+		public override async Task SearchCharacters(SearchRequest request, IServerStreamWriter<CharacterResponse> responseStream, ServerCallContext context)
+		{
+			var results = Characters.Where(c => c.Show.Contains(request.Query, StringComparison.CurrentCultureIgnoreCase));
+			foreach(var result in results)
+			{
+				await responseStream.WriteAsync(new CharacterResponse { Character = result });
+				await Task.Delay(1000);
+			}
+		}
+	}
+}
